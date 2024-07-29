3adac573eed7b3bfa05c5043a4d11fabfa4b2417
lecthh <dev.lecth@gmail.com>
2024-07-29 11:24:36 +0800

    faq page

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.layout')
+
+@section('content')
+
+<div class="flex bg-kDarkBlue px-16 py-4 justify-center">
+    <h1 class="font-Roboto-Condensed text-kDirtyWhite text-xl font-bold">FREQUENTLY ASKED QUESTIONS</h1>
+</div>
+
+<div class="bg-kLightBlue flex flex-col px-16 py-16 gap-y-14">
+    <div class="flex gap-y-[54px]">
+        <div class="flex flex-col gap-y-3 font-Inter">
+            <h1 class="text-kDarkBlue text-xl font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.?</h1>
+            <p class="text-kBlack text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.</p>
+        </div>
+    </div>
+
+    <div class="flex gap-y-[54px]">
+        <div class="flex flex-col gap-y-3 font-Inter">
+            <h1 class="text-kDarkBlue text-xl font-bold">Lorem ipsum dolor sit amet consectetur?</h1>
+            <p class="text-kBlack text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.</p>
+        </div>
+    </div>
+
+    <div class="flex gap-y-[54px]">
+        <div class="flex flex-col gap-y-3 font-Inter">
+            <h1 class="text-kDarkBlue text-xl font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris?</h1>
+            <p class="text-kBlack text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue.</p>
+        </div>
+    </div>
+
+    <div class="flex gap-y-[54px]">
+        <div class="flex flex-col gap-y-3 font-Inter">
+            <h1 class="text-kDarkBlue text-xl font-bold">Lorem ipsum dolor sit amet?</h1>
+            <p class="text-kBlack text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris.</p>
+        </div>
+    </div>
+</div>
+
+<div class="flex bg-kDarkBlue px-16 py-4 justify-center">
+    <h1 class="font-Roboto-Condensed text-kDirtyWhite text-xl font-bold">FREQUENTLY ASKED QUESTIONS</h1>
+</div>
+@endsection
