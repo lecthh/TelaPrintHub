4a8415a7f96bea071aa4e09e063b17fff2ec03dc
lecthh <dev.lecth@gmail.com>
2024-07-30 11:40:55 +0800

    contact us

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,9 +20,51 @@
     </div>
 
     <div class="flex gap-x-6">
-        <div class="bg-kBlack p-3 w-[207px] h-[207px] rounded-md"></div>
-        <div class="bg-kBlack p-3 w-[207px] h-[207px] rounded-md"></div>
-        <div class="bg-kBlack p-3 w-[207px] h-[207px] rounded-md"></div>
+        <div class="bg-kBlack p-3 w-[207px] h-[207px] rounded-md">
+            <img src="/img/landing-1.png" alt="">
+        </div>
+        <div class="bg-kBlack p-3 w-[207px] h-[207px] rounded-md">
+            <img src="/img/landing-2.png" alt="">
+        </div>
+        <div class="bg-kBlack p-3 w-[207px] h-[207px] rounded-md">
+            <img src="/img/landing-3.png" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="bg-kWhite px-8 py-10">
+    <div class="flex items-center justify-center gap-x-[96px]">
+        <img src="/logos/php_logo.png" alt="" class="w-[56]">
+        <img src="/logos/vs_logo.png" alt="" class="w-[56]">
+        <img src="/logos/git_logo.png" alt="" class="w-[56]">
+        <img src="/logos/intel_logo.png" alt="" class="w-[56]">
+        <img src="/logos/spotify_logo.png" alt="" class="w-[56]">
+        <img src="/logos/apple_logo.png" alt="" class="w-[56]">
+        <img src="/logos/microsoft_logo.png" alt="" class="w-[56]">
+    </div>
+</div>
+
+<div class="bg-kWhite flex flex-col gap-y-6 px-8 py-10">
+    <div class="flex gap-x-6 justify-start">
+        <div class="bg-kBlack p-3 rounded-md text-kWhite font-bold text-2xl">
+            How it works
+        </div>
+        <p class="max-w-[1400]">
+            Getting your custom apparel is a breeze with our simple and straightforward process. Follow these easy steps to bring your unique designs to life, choose a reliable printing partner, and enjoy wearing your custom-made clothing with pride.
+        </p>
+    </div>
+
+    <div class="bg-kLightGray p-3 flex flex-col gap-y-4 rounded-md">
+        <h1 class="text-lg font-bold">1. Create a design</h1>
+        <p class="text-base">Start by crafting a design that reflects your style and message. Use our intuitive design tools to create something from scratch or choose from a variety of customizable templates. Upload your own artwork or artwork, ensuring it’s exactly how you envision it.</p>
+    </div>
+    <div class="bg-kLightGray p-3 flex flex-col gap-y-4 rounded-md">
+        <h1 class="text-lg font-bold">2. Choose a partner</h1>
+        <p class="text-base">Select from our curated list of trusted printing partners, each known for their expertise and quality. Compare options based on printing techniques, materials, and customer reviews to find the best fit for your needs. Our partners will ensure your design is printed with precision and care.your own artwork or artwork, ensuring it’s exactly how you envision it.</p>
+    </div>
+    <div class="bg-kLightGray p-3 flex flex-col gap-y-4 rounded-md">
+        <h1 class="text-lg font-bold">3. Wear</h1>
+        <p class="text-base">Once your order is placed, sit back and relax as your custom apparel is expertly produced and delivered. When it arrives, try it on and enjoy your one-of-a-kind garment that’s tailored just for you. Wear it with pride, knowing you’ve created something truly special.</p>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/faq', function() {
-    return view('faq');
+Route::get('/contact', function() {
+    return view('contact');
 });
