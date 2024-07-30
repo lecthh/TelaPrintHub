c3ef99e3e2128c8032fabbca21c879d9228a44b1
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-07-30 21:36:49 +0800

    auth controller and admin controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+{
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    function loginPost(Request $request)
+    {
+    }
+
+    public function register()
+    {
+        $countryCodes = DB::table('country_codes')->get();
+        return view('become-partner', compact('countryCodes'));
+    }
+    public function registerPost(Request $request)
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -7,15 +8,17 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/contact', function() {
+Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/track', function() {
+Route::get('/track', function () {
     return view('track');
 })->name('track');
 
-Route::get('/become-a-partner', function() {
-    $countryCodes = DB::table('country_codes')->get();
-    return view('become-partner', compact('countryCodes'));
-})->name('become-a-partner');
+
+Route::get('/become-a-partner', [AuthController::class, "register"])->name('become-a-partner');
+
+
+Route::get('/login', [AuthController::class, "login"])->name('login');
+Route::post('/login', [AuthController::class, "loginPost"])->name('loginPost');
