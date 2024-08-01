cb47e3f4c3cb261f4716eac36bbb5434bdce8679
lecthh <dev.lecth@gmail.com>
2024-08-02 00:48:22 +0800

    route
    the compact thing sa web or watevah

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PasswordController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -20,8 +19,18 @@ Route::get('/track', function () {
     return view('track');
 })->name('track');
 
+Route::get('/request-print', function () {
+    return view('request.request-1');
+})->name('request-print');
 
-Route::get('/request-print', [HomeController::class, "requestPrint"])->name('request-print');
+Route::get('/request-print-2', function () {
+    return view('request.request-2');
+})->name('request-print');
+
+Route::get('/request-print-3', function () {
+    $countryCodes = DB::table('country_codes')->get();
+    return view('request.request-3', compact('countryCodes'));
+})->name('request-print');
 
 Route::get('/become-a-partner', [AuthController::class, "register"])->name('become-a-partner');
 Route::post('/become-a-partner', [AuthController::class, "registerPost"])->name('become-a-partner.post');
@@ -32,7 +41,6 @@ Route::post('setpassword', [PasswordController::class, "setPassword"])->name('se
 Route::get('/login', [AuthController::class, "login"])->name('login');
 Route::post('/login', [AuthController::class, "loginPost"])->name('login.post');
 
-
-Route::middleware(['auth:admin'])->group(function () {
+Route::middleware("auth.basic")->group(function () {
     Route::view('/catalog', 'partner.catalog')->name('catalog');
 });
