b48ccca707ef16eca6f9be9173dc87cf9c816c46
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-09-02 22:20:09 +0800

    fixes

--- resources/views/request/request-company-selection.blade.php
+++ resources/views/request/request-company-selection.blade.php
@@ -61,8 +61,6 @@
                             </label>
                             @endforeach
                         </div>
-
-                        @endif
                     </div>
                 </div>
                 @endif
@@ -196,16 +194,7 @@
             </div>
         </div>
         @endif
-        <input type="hidden" name="selected_category" id="selected_category" value="{{ request('apparel') }}">
-        <input type="hidden" name="selected_company" id="selected_company">
-        <div class="inline-flex justify-end">
-            <button type="submit" value="Next" class="bg-kBlack font-semibold flex gap-x-2 justify-around items-center text-kWhite text-lg rounded-md p-3">
-                <p>Next</p>
-                <svg width="21" height="9" viewBox="0 0 21 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20.3441 5.11273C20.5445 4.92267 20.5528 4.6062 20.3627 4.40587L17.2656 1.14128C17.0755 0.94095 16.759 0.932622 16.5587 1.12268C16.3584 1.31274 16.35 1.62921 16.5401 1.82954L19.2931 4.7314L16.3913 7.48444C16.191 7.6745 16.1826 7.99097 16.3727 8.1913C16.5627 8.39163 16.8792 8.39996 17.0795 8.2099L20.3441 5.11273ZM0.986847 4.74983L19.9868 5.24982L20.0132 4.25017L1.01315 3.75017L0.986847 4.74983Z" fill="white" />
-                </svg>
-            </button>
-        </div>
+
         </form>
         @if($errors->any())
         <div class="text-red-500 text-sm">
@@ -216,18 +205,16 @@
         @endif
     </div>
 
-
     <script>
         function updateDescription(id, description) {
             document.getElementById('aboutSection').style.display = 'block';
-
+            document.getElementById('selected_company').value = id;
             document.getElementById('company-name').textContent = 'about ' + document.querySelector(`label[for="${id}"] .text-base`).textContent;
             document.getElementById('company-name-2').textContent = document.querySelector(`label[for="${id}"] .text-base`).textContent;
 
             document.getElementById('company-description').textContent = description;
             document.getElementById('comp-desc-2').textContent = description;
-            document.getElementById('selected_company').value = id;
-            document.getElementById('companyAccordion').classList.remove('hidden');
+
         }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
