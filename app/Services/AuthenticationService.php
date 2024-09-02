334f7a20a69d7ac7041f1d2cd5b6f3213be94090
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-09-02 23:02:52 +0800

    validation changes

--- app/Services/AuthenticationService.php
+++ app/Services/AuthenticationService.php
@@ -8,11 +8,10 @@ use App\Models\DesignerCompany;
 use App\Models\DesignerCompanyApparelCategory;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Validation\ValidationException;
 use Webpatser\Uuid\Uuid;
 use Illuminate\Support\Str;
 
-class AuthenticationService
+class AuthenTicationService
 {
     protected $mailService;
 
@@ -23,7 +22,6 @@ class AuthenticationService
 
     public function registerDesignerCompany(array $data)
     {
-        $this->validateUniqueness($data);
 
         $fullPhoneNumber = $data['country_code'] . $data['phone_number'];
 
@@ -57,22 +55,6 @@ class AuthenticationService
         }
 
         $this->mailService->sendVerificationEmail($admin);
-    }
-
-    protected function validateUniqueness(array $data)
-    {
-        if (Admin::where('email', $data['email'])->exists()) {
-            throw ValidationException::withMessages(['email' => 'The email address is already registered.']);
-        }
-
-        $fullPhoneNumber = $data['country_code'] . $data['phone_number'];
-        if (Admin::where('contact_information', $fullPhoneNumber)->exists()) {
-            throw ValidationException::withMessages(['phone_number' => 'The phone number is already registered.']);
-        }
-
-        if (DesignerCompany::where('email', $data['email'])->exists()) {
-            throw ValidationException::withMessages(['email' => 'The designer company with this email is already registered.']);
-        }
     }
 
     protected function sendVerificationEmail(Admin $admin)
