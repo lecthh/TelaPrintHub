6cc224cd7a3de61f2e6746b43f6753c29155f37b
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-09-07 02:15:12 +0800

    logout

--- resources/views/layout/businesshub-layout.blade.php
+++ resources/views/layout/businesshub-layout.blade.php
@@ -219,7 +219,7 @@
                                     profile
                                 </li>
                             </a>
-                            <a href="" class="hover:bg-gray-300 hover:rounded-md p-2">
+                            <a href="{{ route('logout') }}" class="hover:bg-gray-300 hover:rounded-md p-2">
                                 <li class="flex gap-x-2">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
