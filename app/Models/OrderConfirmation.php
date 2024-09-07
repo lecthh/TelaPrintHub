3f8d919cf5633d040bb444de05d610bec67a5f31
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-09-08 01:17:20 +0800

    changed the confirmation

--- app/Models/OrderConfirmation.php
+++ app/Models/OrderConfirmation.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class OrderConfirmation extends Model
 {
     use HasFactory;
+
+    protected $table = 'order_confirmation';
+
+    protected $primaryKey = 'order_confirmation_ID';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'order_confirmation_ID',
+    ];
 }
